e9491f9992174f4195e2241f2f20182028cb7a1a
Srinivasan Ramarao <srinivasan.ramarao@oracle.com>
2018-07-26 16:03:04 -0700

    1st Branch to be updated

---
+++ chapter1/3-Arithmetic_Operators.py
@@ -0,0 +1,37 @@
+num1 = 12
+num2 = 19
+
+#addition
+sumNum= num1+num2
+
+#substraction
+diffNum=num2-num1
+
+#multiplication
+ex1=num1*2
+
+#division
+ex2=num1/2
+
+#modulus
+ex3=num2%2
+
+#print(ex3)
+
+#Adding string
+firstName="Srini"
+lastName="KR"
+
+fullName= firstName+lastName
+print(fullName)
+
+#multiply string
+#print(firstName*5)
+
+sentence="We are learning python"
+
+#extracting string
+print(sentence[0:2])
+print(sentence[:2])
+print(sentence[7:15])
+
