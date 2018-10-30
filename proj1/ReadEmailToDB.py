655e6e808f8fd173fbcc3344ba5e29fb8584d715
rradhakrishnan7 <rajesh_radhakrishnan@apple.com>
2018-10-29 22:03:34 -0700

    updating

--- proj1/ReadEmailToDB.py
+++ proj1/ReadEmailToDB.py
@@ -14,10 +14,7 @@ except ImportError:
 con = cx_Oracle.connect(db_config.user, db_config.pw, db_config.dsn)
 
 cur = con.cursor()
-
 cur1 = con.cursor()
-
-
 
 emailDict=[]
 with open("enron-emails.json") as fh:
@@ -25,25 +22,30 @@ with open("enron-emails.json") as fh:
         emailDict.append(json.loads(line))
 
 
-    # rows={}
+    rows=[]
 
-    cur.execute("select * from email.email")
-    for row in cur.fetchall():
-        print(row)
-
+    # cur.execute("select * from email.email")
+    # for row in cur.fetchall():
+    #     print(row)
+    tup=tuple()
     for i in range(len(emailDict)-1):
+        # try:
         try:
-            #rows = {emailDict[i]["from"], emailDict[i]["subject"]}
-            print(emailDict[i])
-            cur.prepare("INSERT INTO EMAIL.EMAIL(EnronEmailFrm,subject) VALUES (:from, :subject)")
-            print(cur.bindnames())
-
-            cur.executemany(None, emailDict[i])
-            #print("Rows:"+str(rows))
-        except Exception:
+            tup=(emailDict[i]["from"], emailDict[i]["subject"])
+            #print(tup)
+            rows.append(tup)
+            #print(rows)
+            #cur.prepare("INSERT INTO EMAIL.EMAIL(EnronEmailFrm,subject) VALUES (:from, :subject)")
+            #print(cur.bindnames())
+            # except Exception as e:
+            #     print("Exception:",e)
+            #     continue
+            # print(type(rows))
+        except Exception as e:
+            print("Exception for line no:"+str(i)+":",e)
             continue
-        # print(type(rows))
-
+    print("Rows:" + str(rows))
+    cur.executemany("INSERT INTO EMAIL.EMAIL(EnronEmailFrm,subject) VALUES (:from, :subject)", rows)
 
         #cur.execute("""INSERT INTO EMAIL.EMAIL (\"EnronEmailFrm\",\"subject\") VALUES (%s, %s)""",rows)
 
--- proj1/ReadEmilToDB.py
+++ proj1/ReadEmilToDB.py
@@ -3,19 +3,17 @@ import cx_Oracle
 import db_config
 import re
 
-rows = [(1, "First" ), (2, "Second" ),
-         (3, "Third" ), (4, "Fourth" ),
-         (5, "Fifth" ), (6, "Sixth" ),
-         (7, "Seventh" )]
+rows = [("1", "First" ), ("2", "Second" ),
+         ("3", "Third" ), ("4", "Fourth" ),
+         ("5", "Fifth" ), ("6", "Sixth" ),
+         ("7", "Seventh" )]
 
 con = cx_Oracle.connect(db_config.user, db_config.pw, db_config.dsn)
 print("Database version:", con.version)
 
-
-
 cur = con.cursor()
 
-cur.executemany("insert into email.email(From,Subject) values (%s, %s)", rows)
+cur.executemany("""insert into email.email("EnronEmailFrm","Subject") values (:1, :2)""", rows)
 # Now query the results back
 # cur.execute("DELETE FROM mytab")
 cur.execute("commit")
