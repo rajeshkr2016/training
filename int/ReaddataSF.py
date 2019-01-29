2850aae13bbea8f7ae807194e9ea5b6b031c5aa8
rradhakrishnan7 <rajesh_radhakrishnan@apple.com>
2019-01-28 22:23:05 -0800

    updating

--- int/ReaddataSF.py
+++ int/ReaddataSF.py
@@ -1,57 +1,33 @@
 from collections import defaultdict
 import re
-# def readData(sep):
-#     with open("input.data") as data:
-#          for line in data:
-#              print(line)
-#              output=line.split(":")
-#              output1=output[1]
-#              outputidx=output[0]
-#              with open("output.data","w") as odata:
-#                  odata.write(outputidx+sep+output1)
-#
-#
-# readData("-")
-def insertData():
+
+def searchData(**txt):
     empl = defaultdict(list)
     with open("input.data") as data:
         for line in data.readlines():
             output = line.split(":")
             if re.match(r'idx', line):
-                colnames=output[1].split(",")
+                ocol=re.sub(r'\n','', output[1].strip())
+                colnames=ocol.split(",")
                 continue
-
-            outputidx = output[0]
-            output1 = output[1]
-
-            # print(outputidx)
-            #print(output1)
+            outputidx = output[0].strip()
+            output1 = output[1].strip()
             output2 =output1.split(",")
             #print(colnames[0]+":"+ output2[0])
-            empl[outputidx]={colnames[0]:output2[0],colnames[1]:output2[1],colnames[2]:output2[2],colnames[3]:output2[3],colnames[4]:output2[4]}
+            if outputidx in empl[outputidx]:
+                empl[outputidx].append({colnames[0].strip():output2[0].strip(),colnames[1].strip():output2[1].strip(),colnames[2].strip():output2[2].strip(),colnames[3].strip():output2[3].strip(),colnames[4].strip():output2[4].strip()})
+            else:
+                empl[outputidx]={colnames[0].strip(): output2[0].strip(), colnames[1].strip(): output2[1].strip(), colnames[2].strip(): output2[2].strip(), colnames[3].strip(): output2[3].strip(),colnames[4].strip(): output2[4].strip()}
+    #print(empl)
+    count=0
+    for i in range(1,len(empl)+1):
+        #print(empl[str(i)])
+        for skey, svalue in txt.items():
+            if skey in empl[str(i)] and svalue==empl[str(i)][skey]:
+                count+=1
+    if (count > 0):
+        print("Value is present")
+    else:
+        print("value is not present")
 
-                # print(item)
-            # if outputidx in empl[outputidx]:
-            #     #empl[outputidx].append(outputidx)
-            #     for value in output1.split(","):
-            #         if field in empl[outputidx][field]:
-            #             empl[outputidx][field].append(value)
-            #         else:
-            #             empl[outputidx][field]=value
-            # else:
-                #empl[outputidx] = outputidx
-                # for field,value in output1.split(","):
-                #     if field in empl[outputidx][field]:
-                #         empl[outputidx][field].append(value)
-                #     else:
-                #         empl[outputidx][field]=value
-    print(empl)
-
-def search(**txt):
-    for skey, svalue in txt.items():
-        if skey in empl and svalue==empl[skey]:
-            print("Value is present")
-
-#search(fname="xyz")
-
-insertData()
+searchData(fname="John")
